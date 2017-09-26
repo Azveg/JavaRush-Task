5530c94354f4e7190c26bfb048ad7f574578c0da
Azveg <anton.zvegintsev@gmail.com>
2017-09-26 10:08:49 +0300

    Файлы и исключения

---
+++ Task level 18/task1824/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1824;
+
+/* 
+Файлы и исключения
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //Программа должна считывать имена файлов с консоли.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String fileName;
+        FileInputStream fis = null;
+        while (true) {
+            fileName = br.readLine();
+            try {
+                fis = new FileInputStream(fileName);
+                fis.close();
+            } catch (FileNotFoundException e) {
+                br.close();
+                System.out.println(fileName);
+                break;
+            }
+        }
+    }
+
+
+}
