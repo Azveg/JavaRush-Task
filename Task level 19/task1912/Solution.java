34af0805414d82dee5801567402de764d9fad405
Azveg <anton.zvegintsev@gmail.com>
2017-09-26 10:15:39 +0300

    Ридер обертка 2

---
+++ Task level 19/task1912/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task19.task1912;
+
+/* 
+Ридер обертка 2
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream stream = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        testString.printSomething();
+
+        String string = outputStream.toString().replace("te","??");
+
+        System.setOut(stream);
+
+        System.out.println(string);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
